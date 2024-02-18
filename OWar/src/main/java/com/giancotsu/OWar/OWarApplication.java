7469c517e577d85d58f4a267c73cd41a2e2ffae9
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2024-02-18 16:48:14 +0100

    Project init

---
+++ OWar/src/main/java/com/giancotsu/OWar/OWarApplication.java
@@ -0,0 +1,13 @@
+package com.giancotsu.OWar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OWarApplication.class, args);
+	}
+
+}
---
+++ OWar/src/test/java/com/giancotsu/OWar/OWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giancotsu.OWar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
